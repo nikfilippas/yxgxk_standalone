254bf60defbdeb96b1c39bedaec139db4d79df79
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-09-17 16:49:32 +0300

    changed gitignore

--- ymling.py
+++ ymling.py
@@ -1,6 +1,6 @@
 from make_yml import make_yml
 
-fname_data = 'cls_cov_comb.fits'
+fname_data = 'cls_cov.fits'
 
 for i in range(6):
     tname = f'LOWZ__{i}'
@@ -13,5 +13,5 @@ for i in range(6):
              bias_model='HaloModel',
              kmax=1.0,
              fname_data=fname_data,
-             dirname_out=f'yxgxk_{i}',
+             dirname_out=f'yxg_{i}',
              sampler='mcmc', nsamples=30000, debug=False)
