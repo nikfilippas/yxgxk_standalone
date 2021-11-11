68c8b24d619c3589efd5719ea0c02f3fbd804b96
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-11-11 13:27:01 +0200

    changed params

--- plot_triangles.py
+++ plot_triangles.py
@@ -4,9 +4,9 @@ import getdist.mcsamples as gmc
 
 burn = 0.3
 for ibin in range(6):
-    s = gmc.loadMCSamples(f'yxgxk_{ibin}/cobaya', settings={'ignore_rows': burn})
+    s = gmc.loadMCSamples(f'yxgxksig_{ibin}/cobaya', settings={'ignore_rows': burn})
 
     gdplot = gplot.get_subplot_plotter()
-    gdplot.triangle_plot([s], ["sigma8", "ygk_mass_bias", "ygk_rhogy", f"ygk_LOWZ__{ibin}_lMmin_0", f"ygk_LOWZ__{ibin}_lM1_0"], filled=True)
-    plt.savefig("triang.pdf")
+    gdplot.triangle_plot([s], ["sigma8", "ygk_rhogy", "ygk_mass_bias", f"ygk_LOWZ__{ibin}_lMmin_0", f"ygk_LOWZ__{ibin}_lM1_0"], filled=True)
+    plt.savefig(f"triang_yxgxksig_{ibin}.pdf")
     plt.show()
--- ymling.py
+++ ymling.py
@@ -6,12 +6,14 @@ for i in range(6):
     tname = f'LOWZ__{i}'
     make_yml(params_vary=[f'ygk_{tname}_lMmin_0',
                           f'ygk_{tname}_lM1_0',
-                          'ygk_rhogy'],
+                          'ygk_rhogy',
+                          'ygk_mass_bias',
+                          'sigma8'],
              corrs=[(tname, tname),
                     (tname, 'YMILCA'),
                     (tname, 'KAPPA')],
              bias_model='HaloModel',
              kmax=1.0,
              fname_data=fname_data,
-             dirname_out=f'yxgxk_b08_{i}',
+             dirname_out=f'yxgxk_b_uniform_{i}',
              sampler='mcmc', nsamples=30000, debug=False)
