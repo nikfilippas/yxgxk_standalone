e589189d5905c8dbfd44fd1d369c7c53b1ad9f90
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-11-12 17:37:56 +0200

    just run this to avoid having to type everything

---
+++ run_2mpz_kmax05.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+addqueue -s -n 1x12 -q cmb -m 0.5 -c "yxgxksig_0" /users/nikfilippas/.local/bin/cobaya-run chains/yxgxksig/yxgxksig_0/params.yml
+addqueue -s -n 1x12 -q cmb -m 0.5 -c "yxgxk_0" /users/nikfilippas/.local/bin/cobaya-run chains/yxgxk/yxgxk_0/params.yml
+addqueue -s -n 1x12 -q cmb -m 0.5 -c "gxk_0" /users/nikfilippas/.local/bin/cobaya-run chains/gxk/gxk_0/params.yml
+addqueue -s -n 1x12 -q cmb -m 0.5 -c "yxgxk_b08_0" /users/nikfilippas/.local/bin/cobaya-run chains/yxgxk_b08/yxgxk_b08_0/params.yml
+addqueue -s -n 1x12 -q cmb -m 0.5 -c "yxgxk_b_uniform_0" /users/nikfilippas/.local/bin/cobaya-run chains/yxgxk_b_uniform/yxgxk_b_uniform_0/params.yml
+addqueue -s -n 1x12 -q cmb -m 0.5 -c "yxgxk_b_gauss_0" /users/nikfilippas/.local/bin/cobaya-run chains/yxgxk_b_gauss/yxgxk_b_gauss_0/params.yml
