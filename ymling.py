cd6bee65bb616dd095c711b925aa254a4167127c
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-12-08 01:09:14 +0200

    ns_independent

--- ymling.py
+++ ymling.py
@@ -1,6 +1,6 @@
 from make_yml import make_yml
 
-run_name = 'yxgxksig_hmc_hmcode'
+run_name = 'yxgxksig_ns_independent'
 fname_data = 'cls_cov.fits'
 
 for i in range(6):
@@ -16,8 +16,8 @@ for i in range(6):
              bias_model='HaloModel',
              kmax=1.0,
              mass_function="Tinker08",
-             hm_correction="HMCode",
-             ns_independent=False,
+             hm_correction="halofit",
+             ns_independent=True,
              fname_data=fname_data,
              dirname_out=f'chains/{run_name}/{run_name}_{i}',
              sampler='mcmc', nsamples=30000, debug=False)
