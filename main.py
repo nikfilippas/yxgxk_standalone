7bb72289653f8589bb6d5b5853481e4acd0bdfae
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-12-09 22:28:36 +0200

    robustness checks

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ c.plot_tomo(m_bh, "bPe", keep_on=True)
 c.plot_tomo(m_bh, "Omth", keep_on=True)
 
 # systematics
-m_sys = ["yxgxksig", "yxgxk_b_gauss", "gxk",
-         "yxgxksig_hmc_hmcode", "yxgxksig_kmax05",
-         "yxgxksig_mf_despali16", "yxgxgsig_ns_independent"]
+m_sys = ["yxgxksig", "yxgxk_b_gauss",
+         "yxgxksig_kmax05",
+         "yxgxksig_mf_despali16", "yxgxksig_ns_independent"]
 c.plot_tomo(m_sys, "sigma8", keep_on=True)
 c.plot_tomo(m_sys, "ygk_mass_bias", keep_on=True)
 
