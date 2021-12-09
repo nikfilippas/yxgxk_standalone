73a152b34592d8c4db0ef78f9e8fe46c63590508
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-12-09 22:50:18 +0200

    fixed typo

--- ymling.py
+++ ymling.py
@@ -1,6 +1,6 @@
 from make_yml import make_yml
 
-run_name = 'yxgxksig'
+run_name = 'yxgxksig_hmc_hmcode'
 fname_data = 'cls_cov.fits'
 
 for i in range(6):
@@ -17,7 +17,7 @@ for i in range(6):
              bias_model='HaloModel',
              kmax=1.0,
              mass_function="Tinker08",
-             hm_correction="halofit",
+             hm_correction="HMCode",
              ns_independent=False,
              fname_data=fname_data,
              dirname_out=f'chains/{run_name}/{run_name}_{i}',
