9fe32bb6bc02f4cbade6fd9a4db576b5b052b9a8
Nick Koukoufilippas <nikfilippas@gmail.com>
2021-12-14 19:52:29 +0200

    hashing file names

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ from chanal import ChainCalculator
 c = ChainCalculator()
 
 # sigma8
-m_s8 = ["yxgxksig", "yxgxk_b08",
-        "yxgxk_b_uniform", "yxgxk_b_gauss",
-        "gxk", "gxk_kmax05"]
+m_s8 = ["yxgxksig", "gxk", "yxgxk_b08"]
 c.plot_tomo(m_s8, "sigma8", keep_on=True)
 
 # gastrophysics
-m_bh = ["yxgxksig", "yxgxk", "yxgxk_b_uniform",
-        "yxgxk_b_gauss", "yxg", "dam_yxg"]
+m_bh = ["yxgxksig", "yxgxk", "yxg", "yxgxk_b_gauss"]
 c.plot_tomo(m_bh, "ygk_mass_bias", keep_on=True)
 c.plot_tomo(m_bh, "bPe", keep_on=True)
 c.plot_tomo(m_bh, "Omth", keep_on=True)
 
 # systematics
 m_sys = ["yxgxksig", "yxgxk_b_gauss",
-         "yxgxksig_kmax05",
+         "yxgxksig_kmax05", "yxgxksig_hmc_hmcode",
          "yxgxksig_mf_despali16", "yxgxksig_ns_independent"]
 c.plot_tomo(m_sys, "sigma8", keep_on=True)
 c.plot_tomo(m_sys, "ygk_mass_bias", keep_on=True)
+c.plot_tomo(m_sys, "bPe", keep_on=True)
 
 # triangles
 m_tri = ["yxgxksig", "yxgxk_b_uniform", "yxgxk_b_gauss"]
-c.plot_triangles(m_tri)
+c.plot_triangles(m_tri, keep_on=True)
+
+# systematics
+m_tri_sys = ["yxgxksig", "yxgxksig_hmc_hmcode", "yxgxksig_mf_despali16"]
+c.plot_triangles(m_tri_sys, keep_on=True)
 
 # best fits
 c.plot_best_fit("yxgxksig", keep_on=True)
+c.plot_best_fit("yxgxksig_mf_despali16", keep_on=True)
