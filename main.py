939ce56580891106454977ea4dbbf8fbf23d7900
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-02-26 14:25:23 +0200

    all plots

--- main.py
+++ main.py
@@ -3,43 +3,44 @@ from _names import names_new as names
 
 c = ChainCalculator()
 keep_on = True
+overwrite=True
 
 # sigma8
 m_s8 = [names[key] for key in [0, 2, 8, 3]]
-c.plot_tomo(m_s8, "sigma8", keep_on=keep_on)
+c.plot_tomo(m_s8, "sigma8", keep_on=keep_on, overwrite=overwrite)
 
 # gastrophysics
 m_bh = [names[key] for key in [0, 4, 8]]
-c.plot_tomo(m_bh, "ygk_mass_bias", keep_on=keep_on)
-c.plot_tomo(m_bh, "bPe", keep_on=keep_on)
-c.plot_tomo(m_bh, "Omth", keep_on=keep_on)
+c.plot_tomo(m_bh, "ygk_mass_bias", keep_on=keep_on, overwrite=overwrite)
+c.plot_tomo(m_bh, "bPe", keep_on=keep_on, overwrite=overwrite)
+c.plot_tomo(m_bh, "Omth", keep_on=keep_on, overwrite=overwrite)
 c.close_plots()
 
 # gastrophysics mass functions
 m_bh = [names[key] for key in [0, 10, 11, 7]]
-c.plot_tomo(m_bh, "ygk_mass_bias", keep_on=keep_on)
-c.plot_tomo(m_bh, "bPe", keep_on=keep_on)
-c.plot_tomo(m_bh, "Omth", keep_on=keep_on)
+c.plot_tomo(m_bh, "ygk_mass_bias", keep_on=keep_on, overwrite=overwrite)
+c.plot_tomo(m_bh, "bPe", keep_on=keep_on, overwrite=overwrite)
+c.plot_tomo(m_bh, "Omth", keep_on=keep_on, overwrite=overwrite)
 c.close_plots()
 
 # systematics
 m_sys = [names[key] for key in [0, 1, 5, 6, 10]]
-c.plot_tomo(m_sys, "sigma8", keep_on=keep_on)
-c.plot_tomo(m_sys, "ygk_mass_bias", keep_on=keep_on)
-c.plot_tomo(m_sys, "bPe", keep_on=keep_on)
+c.plot_tomo(m_sys, "sigma8", keep_on=keep_on, overwrite=overwrite)
+c.plot_tomo(m_sys, "ygk_mass_bias", keep_on=keep_on, overwrite=overwrite)
+c.plot_tomo(m_sys, "bPe", keep_on=keep_on, overwrite=overwrite)
 c.close_plots()
 
 # triangles
 m_tri = [names[key] for key in [1, 7]]
-c.plot_triangles(m_tri, keep_on=False)
+c.plot_triangles(m_tri, keep_on=keep_on, overwrite=overwrite)
 c.close_plots()
 
 # best fits
-c.plot_best_fit(names[0], keep_on=keep_on)
-c.plot_best_fit(names[7], keep_on=keep_on)
-c.plot_best_fit(names[3], keep_on=keep_on)
+c.plot_best_fit(names[0], keep_on=keep_on, overwrite=overwrite)
+c.plot_best_fit(names[7], keep_on=keep_on, overwrite=overwrite)
+c.plot_best_fit(names[3], keep_on=keep_on, overwrite=overwrite)
 c.close_plots()
 
 # dndz
-c.plot_nz(compare=keep_on, keep_on=keep_on, overwrite=keep_on)
+c.plot_nz(compare=keep_on, keep_on=keep_on, overwrite=overwrite)
 c.close_plots()
