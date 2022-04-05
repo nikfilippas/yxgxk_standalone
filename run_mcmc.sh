17740a79df92a0ce2821195c0fc4671409296927
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-04-05 13:11:54 +0300

    parallelize

--- run_mcmc.sh
+++ run_mcmc.sh
@@ -4,5 +4,5 @@ run_name=$1
 
 for zbin in {0..5}
 do
-  addqueue -s -n 1x4 -q cmb -m 0.5 -c "$run_name"_"$zbin" /users/nikfilippas/.local/bin/cobaya-run chains/$run_name/"$run_name"_"$zbin"/params.yml
+  addqueue -n 8 -q cmb -m 0.5 -c "$run_name"_"$zbin" /users/nikfilippas/.local/bin/cobaya-run chains/$run_name/"$run_name"_"$zbin"/params.yml
 done
