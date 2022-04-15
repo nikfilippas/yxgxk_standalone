7a706153e8ffbf9daaaa1a9b0385f6d10e6095c5
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-04-16 01:25:31 +0300

    new file

--- run_mcmc.sh
+++ run_mcmc.sh
@@ -6,3 +6,6 @@ for zbin in {0..5}
 do
   addqueue -n 8 -q cmb -m 0.5 -c "$run_name"_"$zbin" -o "$run_name"_"$zbin".out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/$run_name/"$run_name"_"$zbin"/params.yml
 done
+
+# *** EXAMPLE ***
+# addqueue -n 8 -q cmb -m 0.5 -c gyksrA_0 -o gyksrA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA/gyksrA_0/params.yml
