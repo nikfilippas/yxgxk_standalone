eca1059b3e0093f8deec3981a85e4908a9219b45
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-04-20 21:09:58 +0300

    minimizers and status reports

--- status.py
+++ status.py
@@ -26,28 +26,53 @@ for model in os.listdir(args.path):
 """
 # List of unconverged chains.
 
-gyksrA_0
+gyksrA_0  # running (wrong dir: chains/gyksrA/gyksrA_0)
+gyksrA_1  # running
 
-gksA_0
-gksA_1
+gyksrA_bG073_0  # running
 
-gykrA_0
-gykrA_1
-gykrA_2
-gykrA_3
-gykrA_4
-gykrA_5
+gksA_0  # running
+gksA_1  # running
 
-gyksr_0
-gyksr_1
-gyksrAAA_0
-gyksrAAA_1
+gykrA_0  # running
 
-gyksrA_SZ_0
-gyksrA_T10_0
+gyksr_0  # running
+gyksr_1  # running
 
-gyksrA_lmin40_1
+gyksrAAA_0  # running
+gyksrAAA_1  # running
+gyksrAAA_2  # running
+gyksrAAA_3  # running
+
+gyksrA_SZ_0  # running
+gyksrA_SZ_1  # running
+
+gyksrA_T10_0  # running
+gyksrA_T10_1  # running
+
+gyksrA_D16_0  # running
+gyksrA_D16_1  # running
+
+gyksrA_B16_0
+gyksrA_B16_1
+
+gyksrA_lmin40_0  # running
+gyksrA_lmin40_1  # running
+gyksrA_lmin40_2  # running
+
 gyksrA_kmax025_0
 gyksrA_kmax025_1
 gyksrA_kmax025_2
+gyksrA_kmax025_3
+gyksrA_kmax025_4
+gyksrA_kmax025_5
+
+gyksrAw_0  # running
+gyksrAw_1  # running
+gyksrAw_2  # running
+gyksrAw_3  # running
+gyksrAw_4  # running
+gyksrAw_5  # running
+
+gyksrA_bU075
 """
