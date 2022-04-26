8b01df88aebce2532939e26c46c54f51fdde6691
Nick Koukoufilippas <nikfilippas@gmail.com>
2022-04-26 18:31:05 +0300

    interpolator and other changes

---
+++ interpolator.py
@@ -0,0 +1,26 @@
+"""Interpolate the sigma8-redshift plane of Omega_grav using Battaglia."""
+import pyccl as ccl
+import numpy as np
+from scipy.interpolate import RBFInterpolator
+from scipy.stats import qmc
+from pressure import BattagliaCalculator
+
+def cosmo_s8(s8):
+    return ccl.Cosmology(
+        Omega_c=0.26066676, Omega_b=0.048974682, h=0.6766, n_s=0.9665,
+        sigma8=s8, transfer_function="bacco")
+
+F_Omgr = BattagliaCalculator(base_model="gyksrA")._get_Omgr
+lo_bounds, hi_bounds = [0.6, 0.05], [1.1, 0.40]
+
+# Set up samples and sample the function.
+sampler = qmc.LatinHypercube(d=2)
+sample = sampler.random(n=1024)
+sample = qmc.scale(sample, lo_bounds, hi_bounds)
+d = np.array([F_Omgr(cosmo_s8(s8), z) for s8, z in sample])
+
+# Build and save interpolator.
+import pickle
+rbf = RBFInterpolator(sample, d, neighbors=20)
+with open("data/Omgr_Battaglia", "wb") as f:
+    pickle.dump(rbf, f)
--- run_mcmc.sh
+++ run_mcmc.sh
@@ -4,36 +4,36 @@ run_name=$1
 
 for zbin in {0..5}
 do
-  addqueue -n 8 -q cmb -m 0.5 -c "$run_name"_"$zbin" -o "$run_name"_"$zbin".out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/$run_name/"$run_name"_"$zbin"/params.yml
+  addqueue -n 1x8 -q cmb -m 0.5 -c "$run_name"_"$zbin" -o "$run_name"_"$zbin".out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/$run_name/"$run_name"_"$zbin"/params.yml
 done
 
 # *** EXAMPLE ***
 """
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_0 -o gyksrA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA/gyksrA_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_1 -o gyksrA_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA/gyksrA_1/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_bG073_0 -o gyksrA_bG073_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_bG073/gyksrA_bG073_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gksA_0 -o gksA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gksA/gksA_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gksA_1 -o gksA_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gksA/gksA_1/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gykrA_0 -o gykrA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gykrA/gykrA_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksr_0 -o gyksr_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksr/gyksr_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksr_1 -o gyksr_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksr/gyksr_1/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrAAA_0 -o gyksrAAA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrAAA_1 -o gyksrAAA_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_1/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrAAA_2 -o gyksrAAA_2.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_2/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrAAA_3 -o gyksrAAA_3.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_3/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_SZ_0 -o gyksrA_SZ_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_SZ/gyksrA_SZ_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_SZ_1 -o gyksrA_SZ_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_SZ/gyksrA_SZ_1/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_T10_0 -o gyksrA_T10_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_T10/gyksrA_T10_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_T10_1 -o gyksrA_T10_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_T10/gyksrA_T10_1/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_lmin40_0 -o gyksrA_lmin40_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_lmin40/gyksrA_lmin40_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_lmin40_1 -o gyksrA_lmin40_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_lmin40/gyksrA_lmin40_1/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_lmin40_2 -o gyksrA_lmin40_2.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_lmin40/gyksrA_lmin40_2/params.yml
-addqueue -n 8 -q berg -m 1.5 -c gyksrAw_0 -o gyksrAw_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_0/params.yml
-addqueue -n 8 -q berg -m 1.5 -c gyksrAw_1 -o gyksrAw_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_1/params.yml
-addqueue -n 8 -q berg -m 1.5 -c gyksrAw_2 -o gyksrAw_2.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_2/params.yml
-addqueue -n 8 -q berg -m 1.5 -c gyksrAw_3 -o gyksrAw_3.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_3/params.yml
-addqueue -n 8 -q berg -m 1.5 -c gyksrAw_4 -o gyksrAw_4.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_4/params.yml
-addqueue -n 8 -q berg -m 1.5 -c gyksrAw_5 -o gyksrAw_5.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_5/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_D16_0 -o gyksrA_D16_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_D16/gyksrA_D16_0/params.yml
-addqueue -n 8 -q cmb -m 1.5 -c gyksrA_D16_1 -o gyksrA_D16_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_D16/gyksrA_D16_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_0 -o gyksrA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA/gyksrA_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_1 -o gyksrA_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA/gyksrA_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_bG073_0 -o gyksrA_bG073_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_bG073/gyksrA_bG073_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gksA_0 -o gksA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gksA/gksA_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gksA_1 -o gksA_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gksA/gksA_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gykrA_0 -o gykrA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gykrA/gykrA_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksr_0 -o gyksr_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksr/gyksr_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksr_1 -o gyksr_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksr/gyksr_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAAA_0 -o gyksrAAA_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAAA_1 -o gyksrAAA_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAAA_2 -o gyksrAAA_2.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_2/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAAA_3 -o gyksrAAA_3.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAAA/gyksrAAA_3/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_SZ_0 -o gyksrA_SZ_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_SZ/gyksrA_SZ_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_SZ_1 -o gyksrA_SZ_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_SZ/gyksrA_SZ_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_T10_0 -o gyksrA_T10_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_T10/gyksrA_T10_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_T10_1 -o gyksrA_T10_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_T10/gyksrA_T10_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_lmin40_0 -o gyksrA_lmin40_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_lmin40/gyksrA_lmin40_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_lmin40_1 -o gyksrA_lmin40_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_lmin40/gyksrA_lmin40_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_lmin40_2 -o gyksrA_lmin40_2.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_lmin40/gyksrA_lmin40_2/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAw_0 -o gyksrAw_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAw_1 -o gyksrAw_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_1/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAw_2 -o gyksrAw_2.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_2/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAw_3 -o gyksrAw_3.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_3/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAw_4 -o gyksrAw_4.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_4/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrAw_5 -o gyksrAw_5.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrAw/gyksrAw_5/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_D16_0 -o gyksrA_D16_0.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_D16/gyksrA_D16_0/params.yml
+addqueue -n 1x8 -q cmb -m 1.5 -c gyksrA_D16_1 -o gyksrA_D16_1.out /users/nikfilippas/.local/bin/cobaya-run chains/chains_new/gyksrA_D16/gyksrA_D16_1/params.yml
 """
